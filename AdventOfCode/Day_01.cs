de28f35a7c7784263a4bde18c9215e67b6c342ef
BuzzardOne <mfbuijse@avans.nl>
2020-12-01 21:12:30 +0100

    Completed Day 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,19 +1,56 @@
 ﻿using AoCHelper;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
+        private readonly List<int> _input;
+        private const int MaxNum = 2020;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToList();
         }
 
-        public override string Solve_1() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 1";
+        public override string Solve_1()
+        {
+            foreach (int i in _input)
+            {
+                if (i > MaxNum)
+                    continue;
 
-        public override string Solve_2() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 2";
+                foreach (int j in _input)
+                {
+                    if (i + j == MaxNum)
+                        return (i*j).ToString();
+                }
+            }
+            throw new SolvingException("No solution found!");
+        }
+
+        public override string Solve_2()
+        {
+            foreach (int i in _input)
+            {
+                if (i > MaxNum)
+                    continue;
+
+                foreach (int j in _input)
+                {
+                    if (j > MaxNum)
+                        continue;
+
+                    foreach (int k in _input)
+                    {
+                        if (i + j + k == MaxNum)
+                            return (i * j * k).ToString();
+                    }
+                }
+            }
+            throw new SolvingException("No solution found!");
+        }
     }
 }
---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,27 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day_02 : BaseDay
+    {
+        private readonly List<int> _input;
+
+        public Day_02()
+        {
+            _input = _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToList();
+        }
+        public override string Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
