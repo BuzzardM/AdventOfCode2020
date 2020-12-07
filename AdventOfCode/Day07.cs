e6475cc1d726a9a3b0359c148d2abe9952a3839b
BuzzardOne <mfbuijse@avans.nl>
2020-12-07 20:47:30 +0100

    added solution day 07

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using AoCHelper;
 
 namespace AdventOfCode
@@ -11,7 +10,9 @@ namespace AdventOfCode
     class Day07 : BaseDay
     {
 
-        private readonly Dictionary<string, List<string>> _input;
+        private readonly Dictionary<string, Bag> _input;
+        private const string BagName = "shiny gold";
+        private readonly Regex _childrenFinderRegex = new Regex(@"(?:[\d].*?) bag+", RegexOptions.Compiled);
 
         public Day07()
         {
@@ -20,17 +21,73 @@ namespace AdventOfCode
 
         public override string Solve_1()
         {
-            throw new NotImplementedException();
+            return GetChildBags(BagName).Count.ToString();
         }
 
         public override string Solve_2()
         {
-            throw new NotImplementedException();
+            return GetChildBagsAmount(BagName).ToString();
         }
 
-        private Dictionary<string, List<string>> ParseInput()
+        private Dictionary<string, Bag> ParseInput()
         {
-            return null;
+            var result = new Dictionary<string, Bag>();
+            foreach (var line in File.ReadAllLines(InputFilePath))
+            {
+                var splitTrim = line.Split("contain");
+                var parentName = splitTrim[0].Replace("bags", "").Trim();
+                var children = splitTrim[1];
+                if (!result.ContainsKey(parentName))
+                    result.Add(parentName, new Bag(parentName));
+
+                foreach (Match child in _childrenFinderRegex.Matches(children))
+                {
+                    var childString = child.Value.Replace("bag", "").Trim();
+                    var amountOfBags = Convert.ToInt32(childString.Substring(0, childString.IndexOf(' ')));
+                    var childName = childString[(childString.IndexOf(' ') + 1)..];
+                    if (!result.TryGetValue(childName, out var bag))
+                    {
+                        bag = new Bag(childName);
+                        result.Add(childName, bag);
+                    }
+
+                    result[parentName].Children.Add(bag, amountOfBags);
+                }
+            }
+
+            return result;
+        }
+
+        private HashSet<Bag> GetChildBags(string bagName)
+        {
+            var result = new HashSet<Bag>();
+            foreach (var bag in _input.Values.Where(bag => bag.Children.Any(children => children.Key.Name == bagName)))
+            {
+                result.Add(bag);
+                foreach (var childBag in GetChildBags(bag.Name))
+                {
+                    result.Add(childBag);
+                }
+            }
+            return result;
+        }
+
+        private int GetChildBagsAmount(string bagName)
+        {
+            var bag = _input[bagName];
+            return bag.Children.Sum(child => child.Value) + bag.Children.Sum(child => child.Value * GetChildBagsAmount(child.Key.Name));
+        }
+
+        internal class Bag
+        {
+            public string Name { get; }
+            public Dictionary<Bag, int> Children { get; }
+
+            public Bag(string name)
+            {
+                Name = name;
+                Children = new Dictionary<Bag, int>();
+            }
         }
     }
 }
