bda041532b01a13ad067ba665147a1496c65d9e3
BuzzardOne <mfbuijse@avans.nl>
2020-12-11 02:07:15 +0100

    added solution day 10 part 2

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -35,7 +35,32 @@ namespace AdventOfCode
 
         public override string Solve_2()
         {
-            throw new NotImplementedException();
+            long amountOfWays = 1;
+            var tempInput = _input;
+            tempInput.Insert(0, 0);
+            var optionalList = GetOptionalList(tempInput).ToList();
+
+            foreach (var item in optionalList)
+            {
+                if (optionalList.Contains(item - 1) && optionalList.Contains(item - 2))
+                    amountOfWays += 3 * amountOfWays / 4;
+                else
+                    amountOfWays += amountOfWays;
+            }
+
+            return amountOfWays.ToString();
+        }
+
+        private static IEnumerable<int> GetOptionalList(IReadOnlyList<int> input)
+        {
+            var result = new List<int>();
+            for (var i = 1; i < input.Count - 1; i++)
+            {
+                if (input[i + 1] - input[i - 1] <= 3)
+                    result.Add(input[i]);
+            }
+
+            return result;
         }
 
         private List<int> ParseInput()
