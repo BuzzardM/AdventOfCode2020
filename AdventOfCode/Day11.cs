ee737dce4f2cbde1ddfbe7e3eee3f6b45f82d8d2
BuzzardOne <mfbuijse@avans.nl>
2020-12-11 15:22:05 +0100

    added template day 11

---
+++ AdventOfCode/Day11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    class Day11 : BaseDay
+    {
+        private char[][] _input;
+
+        public Day11()
+        {
+            _input = ParseInput();
+        }
+
+        public override string Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ChangeSeats(char[][] seats)
+        {
+            
+        } 
+
+        private char[][] ParseInput()
+        {
+            return File.ReadAllLines(InputFilePath).Select(i => i.ToArray()).ToArray();
+        }
+    }
+}
